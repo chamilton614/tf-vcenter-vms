678e898dafe25544bdb959df14e87206147b3870
Chad J. Hamilton <chad_hamilton@anfcorp.com>
2022-02-07 10:44:37 -0500

    Centos8 install template

---
+++ packer/centos8/scripts/package_updates.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eux
+
+echo "package_updates.sh"
+
+#Update packages
+#yum -y update
+dnf -y update
+
+#Install Python3
+# yum -y install python3
+#dnf -y install python3-pip
+
+#Install pip 
+#pip3 install --upgrade pip
+
+#Switch to use Python3 as Default
+alternatives --set python /usr/bin/python3
+
+#Install Ansible
+#pip3 install ansible
+
+#Install epel repo
+dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+#Install Ansible
+dnf -y install --enablerepo epel-playground ansible
