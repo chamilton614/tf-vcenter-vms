f0219de5c2e26dd9891522d3bdda28a268673848
Chad J. Hamilton <chamilton@github.com>
2022-07-20 14:55:02 -0400

    Updated centos-stream 8 and initial terraform ubuntu 2004

--- packer/centos-stream8/package/scripts/cleanup.sh
+++ packer/centos-stream8/package/scripts/cleanup.sh
@@ -3,21 +3,28 @@
 echo "cleanup.sh"
 
 #Remove Ansible
-pip3 uninstall -y ansible
+echo "Removing Ansible"
+#pip3 uninstall -y ansible
+python -m pip uninstall -y ansible
 
 #Clean up Yum
+echo "Clean up Yum"
 yum clean all
 
 #Clear out machine id"
+echo "Clear out machine id"
 rm -f /etc/machine-id
 touch /etc/machine-id
 
 #Clean up /tmp
+echo "Clean up /tmp"
 rm -rf /tmp/*
 
 # Zero out the rest of the free space using dd, then delete the written file.
+echo "Clear out free space"
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+echo "Sync to keep Packer from exiting too soon"
 sync
--- packer/centos-stream8/package/scripts/package_updates.sh
+++ packer/centos-stream8/package/scripts/package_updates.sh
@@ -2,13 +2,24 @@
 
 echo "package_updates.sh"
 
+#Download Stream Repo Files
+#yum -y install centos-release-stream --disablerepo=* --enablerepo='extras'
+
+#Enabling Stream-* allows the download of the latest GPG
+#yum swap centos-{linux,stream}-repos -y --disablerepo=* --enablerepo=extras,Stream-*
+
+#Necessary to use the epel repos
+yum config-manager --set-enabled powertools
+
+#Sync the distro
+yum distro-sync -y
+
 #Update packages
-#yum -y update
-dnf -y update
+yum -y update
 
 #Install Python3
 # yum -y install python3
-dnf -y install python3-pip
+yum -y install python3-pip
 
 #Install pip 
 pip3 install --upgrade pip
@@ -16,11 +27,11 @@ pip3 install --upgrade pip
 #Switch to use Python3 as Default
 alternatives --set python /usr/bin/python3
 
-#Install Ansible
-pip3 install ansible
-
 #Install epel repo
-# dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 
 #Install Ansible
-# dnf -y install --enablerepo epel-playground ansible
+#dnf -y install --enablerepo epel-playground ansible
+#pip3 install ansible
+yum -y install epel-release
+yum -y install ansible
